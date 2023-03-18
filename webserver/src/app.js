08b763205e6fe7f274546088af5542407ec6bf06
otonye77 <otonye.amietubodie@decagon.dev>
2023-03-18 21:56:33 +0100

    hbs

--- webserver/src/app.js
+++ webserver/src/app.js
@@ -6,9 +6,11 @@ const app = express();
 
 const publicDirectoryPath = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, "../templates/views");
+const partialPath = path.join(__dirname, "../templates/partials")
 
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
+hbs.registerPartials(partialPath)
 
 app.use(express.static(publicDirectoryPath));
 
@@ -28,6 +30,7 @@ app.get("/about", (req, res) => {
 
 app.get("/help", (req, res) => {
   res.render("help", {
+    helpText: "This is some helpful text",
     title: "HELP HELP ME HELP ME",
     name: "Sadness",
   });
