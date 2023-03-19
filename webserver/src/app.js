0e6c3e8988ccca5d94a485776da9e2c921e30773
otonye77 <otonye.amietubodie@decagon.dev>
2023-03-19 09:49:49 +0100

    404 page

--- webserver/src/app.js
+++ webserver/src/app.js
@@ -6,11 +6,11 @@ const app = express();
 
 const publicDirectoryPath = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, "../templates/views");
-const partialPath = path.join(__dirname, "../templates/partials")
+const partialPath = path.join(__dirname, "../templates/partials");
 
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
-hbs.registerPartials(partialPath)
+hbs.registerPartials(partialPath);
 
 app.use(express.static(publicDirectoryPath));
 
@@ -33,6 +33,22 @@ app.get("/help", (req, res) => {
     helpText: "This is some helpful text",
     title: "HELP HELP ME HELP ME",
     name: "Sadness",
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404 help",
+    name: "Otonye",
+    errorMessage: "Help article not found",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Otonye",
+    errorMessage: "Page not found",
   });
 });
 
