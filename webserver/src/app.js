6564e06602e7f1b81aaadd7eeb25a51bdef0459f
otonye77 <otonye.amietubodie@decagon.dev>
2023-03-20 10:50:57 +0100

    worked on weather api

--- webserver/src/app.js
+++ webserver/src/app.js
@@ -36,6 +36,30 @@ app.get("/help", (req, res) => {
   });
 });
 
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address",
+    });
+  }
+  res.send({
+    forecast: "It is snowing",
+    location: "Philadelphia",
+    address: req.query.address
+  });
+});
+
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "You must return a search term",
+    });
+  }
+  res.send({
+    products: [],
+  });
+});
+
 app.get("/help/*", (req, res) => {
   res.render("404", {
     title: "404 help",
